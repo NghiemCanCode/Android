e28b92b812b8e9bb9529a0e059b6729c03650159
NghiemCanCode <nghiemgankteam@gmail.com>
2023-03-09 10:48:52 +0700

    10:48 AM 9/3/23

--- app/src/main/java/com/example/android/LoginActivity.java
+++ app/src/main/java/com/example/android/LoginActivity.java
@@ -2,13 +2,26 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView tvBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        tvBack = findViewById(R.id.tvBack);
+
+        tvBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+Button bt_account_2, bt_account_1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bt_account_1 = findViewById(R.id.bt_account_1);
+        bt_account_2 = findViewById(R.id.bt_account_2);
+
+        bt_account_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
